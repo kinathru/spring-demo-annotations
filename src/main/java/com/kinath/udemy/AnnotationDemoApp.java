0ce6d1d288e5371dc599d98417b6c1f8828afde4
kinath <kinath.ru@gmail.com>
2018-05-20 21:57:32 +0530

    implement coach classes

---
+++ src/main/java/com/kinath/udemy/AnnotationDemoApp.java
@@ -0,0 +1,14 @@
+package com.kinath.udemy;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp
+{
+    public static void main( String[] args )
+    {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext( "applicationContext.xml" );
+        Coach tennisCoach = context.getBean( "tennisCoach", Coach.class );
+        System.out.println( tennisCoach.getDailyWorkout() );
+        context.close();
+    }
+}
